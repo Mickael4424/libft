5aa2c0214011907626a0e848960bc39771baf2c8
Mickael4424 <mickaelbouyer87@gmail.com>
2025-11-14 15:54:18 +0100

    new version

--- ft_split.c
+++ ft_split.c
@@ -6,90 +6,121 @@
 /*   By: mbouyer <mbouyer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:34:52 by mbouyer           #+#    #+#             */
-/*   Updated: 2025/11/10 15:21:52 by mbouyer          ###   ########.fr       */
+/*   Updated: 2025/11/14 15:47:19 by mbouyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int word_count(char const *s, char c)
+static int	word_count(const char *s, char c, int total_len)
 {
-    int i;
-    int count;
-    
-    i = 0;
-    count = 0;
-    if (s[i])
-        count++;
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-            count++;
-        i++;
-    }
-    return (count);
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 1;
+	if (s[i] == '\0')
+		return (0);
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+		{
+			i++;
+			if (s[i] != c)
+				word++;
+		}
+		i++;
+	}
+	if (s[0] == c)
+		word--;
+	if (s[total_len - 1] == c)
+		word--;
+	return (word);
+}
+
+void	fill_word(const char *s, char *word, int beg, int end)
+{
+	int	i;
+
+	i = 0;
+	while (beg < end)
+	{
+		word[i] = s[beg];
+		beg++;
+		i++;
+	}
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int		total_word;
+	char	**str;
+	int		i;
+	int		k;
+	int		beg;
+	int		end;	
+
+	if (!s)
+		return (NULL);
+	total_word = word_count(s, c, ft_strlen(s));
+	str = ft_calloc((total_word) + 1, sizeof(char *));
+	if (!str)
+		return (NULL);
+	if (!s || s[0] == '\0')
+	{
+		str[total_word] = NULL;
+		return (str);
+	}
+	i = 0;
+	k = 0;
+	while (i <= (int) ft_strlen(s))
+	{
+		beg = 0;
+		end = 0;
+		while (s[i] == c)
+		{
+			i++;
+			if (s[i] == '\0')
+				return (str);
+		}
+		if (s[i] != c)
+			beg = i;
+		while (s[i] != c)
+		{
+			i++;
+			if (s[i] == '\0')
+			{
+				end = i;
+				break ;
+			}
+		}
+		if (s[i] == c)
+			end = i;
+		str[k] = ft_calloc(end - beg + 1, sizeof(char));
+		if (!str[k])
+			return (0);
+		fill_word(s, str[k], beg, end);
+		i++;
+		k++;
+	}
+	str[total_word] = NULL;
+	return (str);
 }
 /*
-char *fill_word(char const *s_result, in co, char const *s, char c)
-{
-      
-}
-*/
-char **ft_split(char const *s, char c)
-{
-    int len;
-    int count;
-    char **s_res;
-    
-    len = 17;
-    printf("len_s = %d", len);
-    
-    count = word_count(s, c);
-    printf("word cound %d", count);
-    
-    s_res = ft_calloc(count + 1, sizeof(char *));
-    if (!s_res)
-        return (NULL);
-    /*
-    while (i < count)
-    {
-        s_res[i] = (char *) malloc(sizeof(char *) * len);
-        while (s[k] != c)
-        {
-            s_res[i][j] = s[k];
-            j++;
-        }
-        i++;
-        k++;
-    }
-
-
-    while (i < count)
-    {
-        while (s_res[i][j] != c)
-        {
-            s_res[i][j] = s[k];
-            j++;
-            k++;
-        }
-        i++;
-        k++;
-    }
- 
-    i = 0;
-    while (i < count)
-    {
-        printf("s_res %d : %s\n", i, s_res[j]);
-        j++;
-    }
-*/
-    return (s_res);
-}
-
 int main (void)
 {
-    char str[] = "lacviecestcbelle";
-    char ca = 'c';
-    ft_split(str, ca);
-    return (0);
+    char a[] = "";
+    char b = ' ';
+ 	char **res;
+ 	int i;
+	
+ 	i = 0;
+    res = ft_split(a, b);
+	while (res[i])
+	{
+ 		printf("%s\n", res[i]);
+ 		i++;
+ 	}
+ 	return (0);
 }
+*/
